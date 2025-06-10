a530d23bf32b38c6c9d50e9f308fa753406313a9
THAYNÁ DE OLIVEIRA FREIRE <24122484@CFP127ALUNO.LOCAL>
2025-06-10 10:32:55 -0300

    cadastrar viagem feito, pequeno ajuste

--- js/cadastro-viagem.js
+++ js/cadastro-viagem.js
@@ -226,25 +226,6 @@ async function mostrarFotoPerfil() {
   }
 }
 
-// Função para buscar localização pelo nome e país
-async function buscarLocalizacao(nome, id_pais) {
-  try {
-    const response = await fetch('http://10.107.134.21:8080/v1/travello/localizacao')
-    const data = await response.json()
-    
-    if (data.localizacao) {
-      const localizacao = data.localizacao.find(loc => 
-        loc.nome === nome && loc.pais[0].id === Number(id_pais)
-      )
-      return localizacao ? localizacao.id : null
-    }
-    return null
-  } catch (error) {
-    console.error('Erro ao buscar localização:', error)
-    return null
-  }
-}
-
 // cadastrar viagem
 async function cadastrarViagem() {
   const id_usuario = Number(localStorage.getItem('idUser'))
